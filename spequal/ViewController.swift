a7a36df22db6561c71d0604363a9795e012576ea
Lauren <laurencaponong@gmail.com>
2016-04-08 21:48:48 -0400

    Added audio player

--- spequal/ViewController.swift
+++ spequal/ViewController.swift
@@ -6,13 +6,65 @@
 //  Copyright © 2016 Lauren Caponong. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  AVAudioRecorderDelegate {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    var soundRecorder: AVAudioRecorder!
+    var soundPlayer: AVAudioPlayer!
+    let fileName = "demo.caf"
+
+    func getCacheDirectory() -> String {
+    
+        let paths = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true) as [String]
+        
+        return paths[0]
+    }
+    
+    func getFileURL() -> NSURL {
+        let path = getCacheDirectory().stringByAppendingString(fileName)
+        let filePath = NSURL(fileURLWithPath: path)
+        return filePath
+    }
+    
+    func setupRecorder() {
+        var recordSettings : [String : Any] = [
+            AVFormatIDKey: kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey : AVAudioQuality.Max.rawValue,
+            AVEncoderBitRateKey : 320000,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey : 44100.0
+        ]
+        
+        var error: NSError?
+        
+        soundRecorder = AVAudioRecorder(URL: getFileURL(), settings: recordSettings, error: &error)
+        
+        if let err = error {
+            print(error?.localizedDescription)
+        } else {
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func playButtonTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func recordButtonTapped(sender: AnyObject) {
     }
 
     override func didReceiveMemoryWarning() {
