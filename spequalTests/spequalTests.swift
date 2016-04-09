b9b56400df82d52af7fd7ea43e33c635d5447777
Lauren <laurencaponong@gmail.com>
2016-04-08 21:22:39 -0400

    Set up repository and cocoapod

---
+++ spequalTests/spequalTests.swift
@@ -0,0 +1,36 @@
+//
+//  spequalTests.swift
+//  spequalTests
+//
+//  Created by Lauren Caponong on 4/8/16.
+//  Copyright © 2016 Lauren Caponong. All rights reserved.
+//
+
+import XCTest
+@testable import spequal
+
+class spequalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
